c1136f82b45c5a106b587ff6609a2575cf2be277
MHAUSER\mhauser <hauser.michael@gmx.at>
2017-06-11 18:12:10 +0200

    some major changes in trackuino main file: included new sensors, modified trackuino code in order to be able to handle sensors while radio transmission and gps read trackuino saves now sensor values ~ every 0.7 ms  and ~ every 3.5 ms while radio transmission

--- trackuino/trackuino.ino
+++ trackuino/trackuino.ino
@@ -1,21 +1,21 @@
 /* trackuino copyright (C) 2010  EA5HAV Javi
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 
-// Mpide 22 fails to compile Arduino code because it stupidly defines ARDUINO 
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+// Mpide 22 fails to compile Arduino code because it stupidly defines ARDUINO
 // as an empty macro (hence the +0 hack). UNO32 builds are fine. Just use the
 // real Arduino IDE for Arduino builds. Optionally complain to the Mpide
 // authors to fix the broken macro.
@@ -23,7 +23,7 @@
 #error "Oops! We need the real Arduino IDE (version 22 or 23) for Arduino builds."
 #error "See trackuino.pde for details on this"
 
-// Refuse to compile on arduino version 21 or lower. 22 includes an 
+// Refuse to compile on arduino version 21 or lower. 22 includes an
 // optimization of the USART code that is critical for real-time operation
 // of the AVR code.
 #elif (ARDUINO + 0) < 22
@@ -39,6 +39,7 @@
 #include "aprs.h"
 #include "gps.h"
 #include "power.h"
+#include "radio_hx1.h"
 
 // Arduino/AVR libs
 #if (ARDUINO + 1) >= 100
@@ -47,91 +48,140 @@
 #  include <WProgram.h>
 #endif
 
+#include "SPIX0.h"
+#include "SPIX1.h"
+#include "Flash.h"
+#include "Sensors.h"
+
 // Module constants
 static const uint32_t VALID_POS_TIMEOUT = 2000;  // ms
+static uint32_t gps_timeout;
 
 // Module variables
 static int32_t next_aprs = 0;
 
+Sensors sensors;
 
 void setup()
 {
+  //blink led
+  DDRE |= 1 << 2;
+  for (int i = 0; i < 5; i++)
+  {
+    PORTE |= 1 << 2;
+    delay(200);
+    PORTE &= ~(1 << 2);
+    delay(200);
+  }
+
   //5v on
   PORTC |= 1 << 3;
-  DDRC |= (1 << 2);
+  DDRC |= (1 << 3);
 
+  spi0.Init();
+  spi1.Init();
+
+  bool flashErase = flash.Init();
   Serial.begin(GPS_BAUDRATE);
-#ifdef DEBUG_RESET
-  Serial.println("RESET");
-#endif
-
   afsk_setup();
   gps_setup();
+  static RadioHx1 radio;
+  radio.setup();
 
+  sensors.Init();
 
-  Serial.begin(GPS_BAUDRATE);
-  // Do not start until we get a valid time reference
-  // for slotted transmissions.
-  if (APRS_SLOT >= 0) {
-    do {
-      while (! Serial.available())
-        power_save();
-    } while (! gps_decode(Serial.read()));
-    
-    next_aprs = millis() + 1000 *
-      (APRS_PERIOD - (gps_seconds + APRS_PERIOD - APRS_SLOT) % APRS_PERIOD);
+  if (flashErase)
+  {
+    while (flash.RDSR1() & 1); //wait until flash is erased
   }
-  else {
-    next_aprs = millis();
-  }  
-  // TODO: beep while we get a fix, maybe indicating the number of
-  // visible satellites by a series of short beeps?
+
+  gps_reset_parser();
+  gps_timeout = millis();
+  while (gps_fixq == 0) get_pos(); //wait for gps fix
+
+  next_aprs = millis();
 }
 
 void get_pos()
 {
   // Get a valid position from the GPS
   int valid_pos = 0;
-  uint32_t timeout = millis();
 
-#ifdef DEBUG_GPS
-  Serial.println("\nget_pos()");
-#endif
+  if (millis() - gps_timeout > VALID_POS_TIMEOUT)
+  {
+    gps_reset_parser();
+    gps_timeout = millis();
+  }
 
-  gps_reset_parser();
-
-  do {
-    if (Serial.available())
-      valid_pos = gps_decode(Serial.read());
-  } while ( (millis() - timeout < VALID_POS_TIMEOUT) && ! valid_pos) ;
+  while (Serial.available() && !valid_pos)
+  {
+    valid_pos = gps_decode(Serial.read());
+  }
 
   if (valid_pos) {
-
+    saveGPSPoint();
+    gps_reset_parser();
   }
 }
 
 void loop()
 {
+  get_pos();
+  
+  sensors.Handle();
+  
   // Time for another APRS frame
   if ((int32_t) (millis() - next_aprs) >= 0) {
-    get_pos();
     aprs_send();
     next_aprs += APRS_PERIOD * 1000L;
     while (afsk_flush()) {
-      power_save();
+      sensors.HandleFastMode();
     }
 
 #ifdef DEBUG_MODEM
     // Show modem ISR stats from the previous transmission
     afsk_debug();
 #endif
-
-  } else {
-    // Discard GPS data received during sleep window
-    while (Serial.available()) {
-      Serial.read();
-    }
   }
-
-  power_save(); // Incoming GPS data or interrupts will wake us up
 }
+
+union floatConvert
+{
+  float fl;
+  byte bytes[4];
+};
+
+void saveGPSPoint()
+{
+  floatConvert conv;
+  byte point[POINT_LEN_GPS];
+  uint32_t timeStamp = micros();
+
+  point[0] = POINT_INDICATOR_GPS;
+
+  point[1] = timeStamp >> 24;
+  point[2] = timeStamp >> 16;
+  point[3] = timeStamp >> 8;
+  point[4] = timeStamp;
+
+  point[5] = gps_seconds >> 24;
+  point[6] = gps_seconds >> 16;
+  point[7] = gps_seconds >> 8;
+  point[8] = gps_seconds;
+
+  conv.fl = atof(gps_aprs_lat);
+  if (gps_aprs_lat[7] != 'N') conv.fl = -conv.fl;
+  memcpy(point + 9, conv.bytes, 4);
+
+  conv.fl = atof(gps_aprs_lon);
+  if (gps_aprs_lon[8] != 'E') conv.fl = -conv.fl;
+  memcpy(point + 13, conv.bytes, 4);
+
+  conv.fl = gps_altitude;
+  memcpy(point + 17, conv.bytes, 4);
+
+  point[21] = gps_satellites;
+
+  flash.Write(point, POINT_LEN_GPS);
+}
+
